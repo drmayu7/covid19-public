dc220956f65003f7af7d1580ab1c804638bf1731
drmayu <116558862+drmayu@users.noreply.github.com>
2022-12-10 00:19:27 +0800

    Added heatmap visual

--- get_visual.py
+++ get_visual.py
@@ -2,3 +2,43 @@ def plot_clustercategory_bar(df):
     category = df.groupby('category').count()['cluster'].sort_values(ascending=False)
     category.plot(kind='bar', color=['darkslateblue', 'midnightblue', 'royalblue', 'deepskyblue', 'lightskyblue',
                                         'mediumturquoise', 'paleturquoise'])
+
+def plot_clustertrend_heatmap(df):
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    sns.set_theme()
+    import warnings
+    warnings.filterwarnings('ignore')
+
+    #Dataframe Transformation
+    cluster_casedist = df[['cluster','date_announced','category','cases_total']]
+    cluster_casedist['date_announced'] = pd.to_datetime(cluster_casedist['date_announced'],yearfirst=True)
+
+    #Pivot
+    cluster_pivot = cluster_casedist.pivot_table(index=pd.Grouper(freq = 'W',key='date_announced'),columns='category',values='cases_total',aggfunc=np.sum,fill_value=0)
+    cluster_pivot_week = cluster_pivot.resample('7D').sum().tail(25)
+
+    #Heatmap Visualization
+    f,ax = plt.subplots(figsize=(19, 25))
+    y_axis_label = ['24 weeks ago','23 weeks ago','22 weeks ago','21 weeks ago','20 weeks ago','19 weeks ago','18 weeks ago','17 weeks ago','16 weeks ago','15 weeks ago','14 weeks ago','13 weeks ago','12 weeks ago','11 weeks ago','10 weeks ago','9 weeks ago','8 weeks ago','7 weeks ago','6 weeks ago','5 weeks ago','4 weeks ago','3 weeks ago','2 weeks ago','1 weeks ago','This week']
+    plt.tick_params(axis='both', which='major', labelsize='large', labelbottom = False, bottom=False, top = False, labeltop=True)
+    f.suptitle('Distribution of Cases in Clusters by Clusters Category',fontsize = 20,y=0.92)
+
+    sns.heatmap(
+        cluster_pivot_week,
+        annot=True,
+        annot_kws = {'size':15},
+        fmt=".0f",
+        linewidths=.5,
+        ax=ax,
+        linecolor ='black',
+        cmap='Reds',
+        cbar = False,
+        yticklabels=y_axis_label
+    ).invert_yaxis()
+
+    ax.set_ylabel(ylabel = '')
+    ax.set_xlabel(xlabel ='Cases in Clusters Distribution',loc = 'center')
